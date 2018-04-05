b73acde1a99acfd3fc3b8a5fa087b9b4b2285cd7
Venkatesan, Kirubanandam <kiruba_vishwa@yahoo.co.in>
2018-04-05 15:09:09 +0530

    demo

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World!");
-		System.out.println("Change text in file to trigger job in Jenkins");
-		System.out.println("Proxy changed in git");
+		System.out.println("Demo session!");
+		
 	}
 }
