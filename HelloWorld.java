c42051f3caa5842cf9b1b06f785f507ebe07a8f1
Venkatesan, Kirubanandam <kiruba_vishwa@yahoo.co.in>
2018-04-05 14:54:06 +0530

    added new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld
 	{
 		System.out.println("Hello World!");
 		System.out.println("Change text in file to trigger job in Jenkins");
+		System.out.println("Proxy changed in git");
 	}
 }
